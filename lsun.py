d1e01944ed60a977cba176edbfc6a4a61a856722
ironhead <lronheadChuang@ironhead.local>
2017-02-05 18:38:47 +0800

    - initial commit for DCGAN on LSUN

---
+++ lsun.py
@@ -0,0 +1,63 @@
+"""
+"""
+import lmdb
+import numpy
+import scipy.misc
+import StringIO
+
+
+class Lsun(object):
+    """
+    """
+    def __init__(self, path_lsun_dir):
+        """
+        """
+        self._lmdb_path = path_lsun_dir
+        self._lmdb_keys = []
+
+        with lmdb.open(self._lmdb_path) as env:
+            with env.begin(write=False) as txn:
+                with txn.cursor() as cursor:
+                    self._lmdb_keys = [k for k, v in cursor]
+
+        self._key_indice = numpy.arange(len(self._lmdb_keys))
+        self._key_position = 0
+
+        numpy.random.shuffle(self._key_indice)
+
+    def next_batch(self, batch_size):
+        """
+        """
+        begin = self._key_position
+
+        self._key_position += batch_size
+
+        if self._key_position > len(self._key_indice):
+            numpy.random.shuffle(self._key_indice)
+
+            begin = 0
+
+            self._key_position = batch_size
+
+            assert batch_size <= len(self._key_indice)
+
+        end = self._key_position
+
+        images = []
+
+        with lmdb.open(self._lmdb_path) as env:
+            with env.begin(write=False) as txn:
+                with txn.cursor() as cursor:
+                    for i in xrange(begin, end):
+                        val = cursor.get(self._lmdb_keys[self._key_indice[i]])
+
+                        sio = StringIO.StringIO(val)
+
+                        img = scipy.misc.imread(sio).astype(numpy.float32)
+
+                        img /= 127.5
+                        img -= 1.0
+
+                        images.append(img)
+
+        return images
---
+++ model.py
@@ -0,0 +1,211 @@
+"""
+implement DCGAN with Tensorflow for MINST.
+"""
+import tensorflow as tf
+
+
+class Dcgan(object):
+    @staticmethod
+    def leaky_relu(x, leak=0.2, name="lrelu"):
+        """
+        https://github.com/tensorflow/tensorflow/issues/4079
+        """
+        with tf.variable_scope(name):
+            f1 = 0.5 * (1.0 + leak)
+            f2 = 0.5 * (1.0 - leak)
+            return f1 * x + f2 * abs(x)
+
+    @staticmethod
+    def discriminator(source, reuse):
+        """
+        build the discriminator network.
+        param 'source' is the input data in shape [-1, 256, 256, 3].
+        param 'resue' is for sharing the network with generator.
+        """
+        weights_initializer = tf.truncated_normal_initializer(stddev=0.02)
+
+        # # arXiv:1511.06434v2
+        # build convolutional net to downsample.
+        # no pooling layers.
+        for layer_idx in xrange(4):
+            # arXiv:1511.06434v2
+            # in discriminator, use batch norm except input layer
+            if layer_idx == 0:
+                normalizer_fn = None
+            else:
+                normalizer_fn = tf.contrib.layers.batch_norm
+
+            # arXiv:1511.06434v2
+            # in discriminator, use LeakyReLU
+            source = tf.contrib.layers.convolution2d(
+                inputs=source,
+                num_outputs=2 ** (7 + layer_idx),
+                kernel_size=[4, 4],
+                stride=[2, 2],
+                padding='SAME',
+                activation_fn=Dcgan.leaky_relu,
+                normalizer_fn=normalizer_fn,
+                weights_initializer=weights_initializer,
+                scope='d_conv_{}'.format(layer_idx),
+                reuse=reuse)
+
+        # for fully connected layer
+        source = tf.contrib.layers.flatten(source)
+
+        # fully connected layer to binary classify
+        source = tf.contrib.layers.fully_connected(
+            inputs=source,
+            num_outputs=1,
+            activation_fn=tf.nn.sigmoid,
+            weights_initializer=weights_initializer,
+            scope='d_out',
+            reuse=reuse)
+
+        return source
+
+    @staticmethod
+    def generator(seed):
+        """
+        build the generator network.
+        """
+        weights_initializer = tf.truncated_normal_initializer(stddev=0.02)
+
+        # fully connected layer to upscale the seed for the input of
+        # convolutional net.
+        target = tf.contrib.layers.fully_connected(
+            inputs=seed,
+            num_outputs=4 * 4 * 1024,
+            activation_fn=tf.nn.relu,
+            normalizer_fn=tf.contrib.layers.batch_norm,
+            weights_initializer=weights_initializer,
+            scope='g_project')
+
+        # reshape to images
+        target = tf.reshape(target, [-1, 4, 4, 1024])
+
+        # transpose convolution to upscale
+        for layer_idx in xrange(6):
+            if layer_idx == 5:
+                num_outputs = 3
+
+                # arXiv:1511.06434v2
+                # use tanh in output layer
+                activation_fn = tf.nn.tanh
+
+                # arXiv:1511.06434v2
+                # use batch norm except the output layer
+                normalizer_fn = None
+            else:
+                num_outputs = 2 ** (9 - layer_idx)
+
+                # arXiv:1511.06434v2
+                # use ReLU
+                activation_fn = tf.nn.relu
+
+                # arXiv:1511.06434v2
+                # use batch norm
+                normalizer_fn = tf.contrib.layers.batch_norm
+
+            target = tf.contrib.layers.convolution2d_transpose(
+                inputs=target,
+                num_outputs=num_outputs,
+                kernel_size=5,
+                stride=2,
+                padding='SAME',
+                activation_fn=activation_fn,
+                normalizer_fn=normalizer_fn,
+                weights_initializer=weights_initializer,
+                scope='g_conv_t_{}'.format(layer_idx))
+
+        return target
+
+    def __init__(self, params):
+        """
+        build DCGAN in tensorflow.
+        """
+        generator_seed_size = params['generator_seed_size']
+
+        # the input batch placeholder for the generator.
+        self._seed = tf.placeholder(
+            shape=[None, generator_seed_size], dtype=tf.float32)
+
+        # the input batch placeholder (eal data) for the discriminator.
+        self._real = tf.placeholder(shape=[None, 256, 256, 3], dtype=tf.float32)
+
+        # build the generator to generate images from random seeds.
+        self._generate_fake = Dcgan.generator(self._seed)
+
+        # build the discriminator to judge the real data.
+        discriminate_real = Dcgan.discriminator(self._real, False)
+
+        # build the discriminator to judge the fake data.
+        # judge both real and fake data with the same network (shared).
+        discriminate_fake = Dcgan.discriminator(self._generate_fake, True)
+
+        self._loss_discriminator = -tf.reduce_mean(
+            tf.log(discriminate_real) + tf.log(1.0 - discriminate_fake))
+        self._loss_generator = -tf.reduce_mean(tf.log(discriminate_fake))
+
+        trainable_variables = tf.trainable_variables()
+
+        # peek the trainable variables.
+        # trainable_variables[:10] are for generator.
+        # trainable_variables[10:] are for discriminator.
+        # building 2 groups base on their names should be safer.
+        for idx, variable in enumerate(trainable_variables):
+            print idx, variable.name
+
+        trainer_generator = tf.train.AdamOptimizer(
+            learning_rate=0.0002, beta1=0.5)
+        trainer_discriminator = tf.train.AdamOptimizer(
+            learning_rate=0.0002, beta1=0.5)
+
+        # minimize the loss to train the generator and discriminator.
+        gradients_generator = trainer_generator.compute_gradients(
+            self._loss_generator, trainable_variables[:14])
+        gradients_discriminator = trainer_discriminator.compute_gradients(
+            self._loss_discriminator, trainable_variables[14:])
+
+        self._train_generator = trainer_generator.apply_gradients(
+            gradients_generator)
+        self._train_discriminator = trainer_discriminator.apply_gradients(
+            gradients_discriminator)
+
+        self._session = tf.Session()
+
+        self._session.run(tf.global_variables_initializer())
+
+    def train_discriminator(self, seed_sources, real_sources):
+        """
+        train the discriminator network with batch data.
+        the generator use seed_sources to generate fake_sources for the
+        discriminator.
+        """
+        fetch = [self._train_discriminator, self._loss_discriminator]
+        feeds = {self._seed: seed_sources, self._real: real_sources}
+
+        _, loss = self._session.run(fetch, feed_dict=feeds)
+
+        return loss
+
+    def train_generator(self, seed_sources):
+        """
+        train the generator network with batch data.
+        """
+        fetch = [self._train_generator, self._loss_generator]
+        feeds = {self._seed: seed_sources}
+
+        _, loss = self._session.run(fetch, feed_dict=feeds)
+
+        return loss
+
+    def generate(self, seed_sources):
+        """
+        generate fake images from the seeds.
+        """
+        fetch = self._generate_fake
+        feeds = {self._seed: seed_sources}
+
+        fake = self._session.run(fetch, feed_dict=feeds)
+
+        return fake
